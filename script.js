49068db361ec821e6f6e04160d7a40bb9bdbb7c2
bryxzi <122295924+bryxzi@users.noreply.github.com>
2023-01-26 23:37:12 -0500

    create container's for values in the time array, add event listener to store, then retrieve values

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
-
-var dayWeek = dayjs().format('[Today is] dddd');
-console.log(dayWeek)
-$('#currentDay').text(dayWeek);
-
-var currentHour = dayjs().hour();
-console.log(currentHour);
-
 
 function evaluateOver(time){
   if(currentHour > time){
@@ -39,29 +8,43 @@ function evaluateOver(time){
     return "future"
   }
 }
-evaluateOver(15)
+var currentHour = dayjs().hour();
+var dayWeek = dayjs().format('[Today is] dddd');
+var todaysDay = document.getElementById('currentDay');
+
+//display the day of the week in the header
+todaysDay.innerHTML = `<h1>${dayWeek}</h1>`
 
 
+//array of all the working hours in a day
+var times = ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM","1:00 PM","2:00 PM","3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM","7:00 PM", "8:00 PM", "9:00 PM"]
 
 
+//takes the html example to loop and iterate over array times, adds unique ID, class and text to each row
+for(let i=0; i< times.length; i++){
+  document.getElementById("container").innerHTML += 
+  `<div id="hour-${i+9}" class="row time-block ${evaluateOver(i+9)}">
+  <div class="col-2 col-md-1 hour text-center py-3">${times[i]}</div>
+  <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+  <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+  <i class="fas fa-save" aria-hidden="true"></i>
+  </button>
+  </div>
+  `
+}
 
+//added event listener to save button and stores values in local storage
+$(document).on('click', '.saveBtn', function () {
+  var timeBlockId = $(this).closest('.time-block').attr('id');
+  var description = $(this).siblings('.description').val();
+  localStorage.setItem(timeBlockId, description);
+})
 
-
-
-
-
-
-// var todaysDay = document.getElementById('currentDay');
-// todaysDay.innerHTML = `<h1>${dayWeek}</h1>`
-// var times = ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM","fasd","fsadf","Fasdf", "time"]
-// for(let i=0; i< times.length; i++){
-// document.getElementById("container").innerHTML += 
-// `<div id="hour-${i+9}" class="row time-block ${evaluateOver(i+9)}">
-//   <div class="col-2 col-md-1 hour text-center py-3">${times[i]}</div>
-//   <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
-//   <button class="btn saveBtn col-2 col-md-1" aria-label="save">
-//   <i class="fas fa-save" aria-hidden="true"></i>
-// </button>
-// </div>
-// `
-// }
+//iterates over each time-block element, gets its id attribute, uses it as a key to retrieve the corresponding value from localStorage, and sets the value of the textarea element inside the time-block to the retrieved value
+$('.time-block').each(function() {
+  var timeBlockId = $(this).attr('id');
+  var savedInput = localStorage.getItem(timeBlockId);
+  if (savedInput) {
+    $(this).find('.description').val(savedInput);
+  }
+});
