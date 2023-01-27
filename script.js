1ac6d46356ada939a1088737c62f631f6c3c57d2
bryxzi <122295924+bryxzi@users.noreply.github.com>
2023-01-26 19:38:02 -0500

    added current day to header

--- script.js
+++ script.js
@@ -21,3 +21,47 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+var dayWeek = dayjs().format('[Today is] dddd');
+console.log(dayWeek)
+$('#currentDay').text(dayWeek);
+
+var currentHour = dayjs().hour();
+console.log(currentHour);
+
+
+function evaluateOver(time){
+  if(currentHour > time){
+    return "past"
+  } else if (currentHour == time){
+    return "present"
+  } else{
+    return "future"
+  }
+}
+evaluateOver(15)
+
+
+
+
+
+
+
+
+
+
+
+// var todaysDay = document.getElementById('currentDay');
+// todaysDay.innerHTML = `<h1>${dayWeek}</h1>`
+// var times = ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM","fasd","fsadf","Fasdf", "time"]
+// for(let i=0; i< times.length; i++){
+// document.getElementById("container").innerHTML += 
+// `<div id="hour-${i+9}" class="row time-block ${evaluateOver(i+9)}">
+//   <div class="col-2 col-md-1 hour text-center py-3">${times[i]}</div>
+//   <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+//   <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+//   <i class="fas fa-save" aria-hidden="true"></i>
+// </button>
+// </div>
+// `
+// }
